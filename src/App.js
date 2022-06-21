055239607191fb1534a84650d008fc02988b340c
ocean-p <duongkodoiqua@gmail.com>
2022-06-21 12:00:36 +0700

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import NavBar from './NavBar/NavBar';
 
 function App() {
   return (
-    <div className='h-screen pl-4'>
-      Hello World
+    <div className='h-screen px-4 py-8 font-sora text-white
+      bg-gradient-to-b from-purple-900 to-purple-700
+    '>
+      <NavBar/>
     </div>
   );
 }
