1b8cf6aefef43f6420e9e141f17ae41f64eeda33
ocean-p <duongkodoiqua@gmail.com>
2022-06-21 12:33:50 +0700

    create slogan component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import NavBar from './NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
+import Slogan from './components/Slogan/Slogan';
 
 function App() {
   return (
     <div className='h-screen px-4 py-8 font-sora text-white
       bg-gradient-to-b from-purple-900 to-purple-700
+      overflow-y-auto overflow-x-hidden
     '>
       <NavBar/>
+      <Slogan/>
     </div>
   );
 }
