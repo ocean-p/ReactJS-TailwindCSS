825bb8c7a2621c986f6f460d0577374d68000285
ocean-p <duongkodoiqua@gmail.com>
2022-06-21 14:59:08 +0700

    add click event on menu icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BsSunFill} from 'react-icons/bs';
 import {HiOutlineMenu} from 'react-icons/hi';
+import {MdOutlineClose} from 'react-icons/md';
 
 const NavBar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenu = () => {
+    setOpenMenu(!openMenu);
+  }
+
   return (
     <nav className='flex items-center'>
       <div className='flex items-center'>
@@ -10,7 +17,27 @@ const NavBar = () => {
         <BsSunFill size={'24px'} color='#e9c46a' className='cursor-pointer'/>
       </div>
       <ul className='ml-auto text-16 font-semibold'>
-        <HiOutlineMenu size={'24px'} className='cursor-pointer'/>
+        {openMenu ? 
+          <MdOutlineClose
+            size={'24px'}
+            className='cursor-pointer'
+            onClick={handleMenu}
+          /> :
+          <HiOutlineMenu 
+            size={'24px'} 
+            className='cursor-pointer'
+            onClick={handleMenu}
+          />
+        }
+        {openMenu &&
+          <div className='absolute right-8 bg-white p-8 
+          text-center text-13 text-black'>
+            <li className='cursor-pointer'>Features</li>
+            <li className='cursor-pointer'>Menu</li>
+            <li className='cursor-pointer'>Our Story</li>
+            <li className='cursor-pointer'>Contact</li>
+          </div>
+        }
       </ul>
     </nav>
   )
