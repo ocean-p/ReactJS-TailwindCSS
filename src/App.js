742569cd764122a2432ea9ae78f7f1838960656b
ocean-p <duongkodoiqua@gmail.com>
2022-06-22 11:33:34 +0700

    make responsive navbar in pc screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Content from './components/Content/Content';
 import NavBar from './components/NavBar/NavBar';
 import Slogan from './components/Slogan/Slogan';
 
 function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined
+  });
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleSize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    };
+    window.addEventListener('resize', handleSize);
+    return () => window.removeEventListener('resize', handleSize);
+  }, []);
+
+  useEffect(() => {
+    if(windowSize.width < 500){
+      setIsMobile(true)
+    }
+    else{
+      setIsMobile(false)
+    }
+    console.log(windowSize)
+  }, [windowSize])
+
   return (
     <div className='h-screen px-4 py-8 font-sora text-white
       bg-gradient-to-b from-purple-900 to-purple-700
       overflow-y-auto overflow-x-hidden
     '>
-      <NavBar/>
+      <NavBar isMobile={isMobile}/>
       <Slogan/>
       <Content/>
     </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import {BsSunFill} from 'react-icons/bs';
-import {HiOutlineMenu} from 'react-icons/hi';
-import {MdOutlineClose} from 'react-icons/md';
+import { BsSunFill } from 'react-icons/bs';
+import { HiOutlineMenu } from 'react-icons/hi';
+import { MdOutlineClose } from 'react-icons/md';
 
-const NavBar = () => {
+const NavBar = ({ isMobile }) => {
   const [openMenu, setOpenMenu] = useState(false);
 
   const handleMenu = () => {
@@ -14,20 +14,28 @@ const NavBar = () => {
     <nav className='flex items-center'>
       <div className='flex items-center'>
         <div className='text-20 font-bold mr-2'>NerdCard</div>
-        <BsSunFill size={'24px'} color='#e9c46a' className='cursor-pointer'/>
+        <BsSunFill size={'24px'} color='#e9c46a' className='cursor-pointer' />
       </div>
-      <ul className='ml-auto text-16 font-semibold'>
-        {openMenu ? 
+      <ul className='md:flex md:gap-10 ml-auto text-16 font-semibold'>
+        {openMenu && isMobile ?
           <MdOutlineClose
             size={'24px'}
             className='cursor-pointer'
             onClick={handleMenu}
-          /> :
-          <HiOutlineMenu 
-            size={'24px'} 
-            className='cursor-pointer'
-            onClick={handleMenu}
-          />
+          /> : !openMenu && isMobile ?
+            <HiOutlineMenu
+              size={'24px'}
+              className='cursor-pointer'
+              onClick={handleMenu}
+            />
+            : (
+              <>
+                <li className='btn-hover'>Features</li>
+                <li className='btn-hover'>Menu</li>
+                <li className='btn-hover'>Our Story</li>
+                <li className='btn-hover ml-28'>Contact</li>
+              </>
+            )
         }
         {openMenu &&
           <div className='absolute right-8 bg-white p-8 
